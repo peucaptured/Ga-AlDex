6c088b66bcaacedb8c224acdfe38abf64ee45644
peucaptured <114812163+peucaptured@users.noreply.github.com>
2026-02-09 17:06:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17982,7 +17982,8 @@ elif page == "PvP – Arena Tática":
                             icon = icons[idx] if idx < len(icons) else "⚪"
 
                             with st.expander(f"{icon} {opp_name}", expanded=True):
-                                render_player_column(db, rid, opp_name, room, is_player=False)
+                                render_player_column(opp_name, "", is_me=False)
+
 
             # =========================
             # 8. LÓGICA DE CLIQUE
